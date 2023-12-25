132afdce722b33712b5f683e53393a1a609881fb
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2023-12-26 01:25:31 +0200

    reservations

--- schoolsadmin/urls.py
+++ schoolsadmin/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('school_selection/', views.school_selection, name='school_selection'),
     path('all_schools/', views.all_schools, name='all_schools'),
     path('schools_per_department/<int:department_id>/', views.schools_per_department, name='schools_per_department'),
+    path('schools_details/<int:school_id>/', views.schools_details, name='schools_details'),
     # path('add/', views.SchoolUserCreateView, name='schooluser_add'),
     # path('delete/<int:school_id>/', views.SchoolUserDeleteView, name='schooluser_delete'),
     # path('update/<int:school_id>/', views.SchoolUserUpdateView, name='schooluser_update'),
--- schoolsadmin/views.py
+++ schoolsadmin/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
-from schools.models import School, SchoolUser
+from schools.models import School, SchoolUser, Department
 from schools.forms import SchoolUserForm
 
 @login_required
@@ -14,13 +14,15 @@ def school_selection(request):
     #school_id = request.GET.get('school')
 
     if request.GET.get('filter') == '1' and department_id:
+        print(department_id)
+        print(request.GET.get('filter'))
         # Reset selected values
         form = SchoolUserForm()
         return redirect('schoolsadmin:schools_per_department', department_id=department_id)
     
     if request.GET.get('filter') == '1' and department_id is '':
         form = SchoolUserForm()
-        return redirect('schoolsadmin:allschools')
+        return redirect('schoolsadmin:all_schools')
 
     context.update({
         'form': form
@@ -31,9 +33,37 @@ def school_selection(request):
 @login_required
 @user_passes_test(lambda u: u.is_superuser)
 def all_schools(request):
-    pass
+
+    school_users = SchoolUser.objects.all().order_by('school__name')
+
+    context = {
+        'school_users': school_users,
+    }
+
+    return render(request, 'schoolsadmin/all_schools.html', context)
 
 @login_required
 @user_passes_test(lambda u: u.is_superuser)
 def schools_per_department(request, department_id):
-    pass
+    
+    dept = get_object_or_404(Department, id=department_id)
+    school_users = SchoolUser.objects.filter(department=dept).order_by('department__name', 'school__name')
+
+    context = {
+        'dept': dept,
+        'school_users': school_users,
+    }
+
+    return render(request, 'schoolsadmin/schools_per_department.html', context)
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def schools_details(request, school_id):
+    
+    school_user = SchoolUser.objects.get(id=school_id)
+
+    context = {
+        'school_user': school_user,
+    }
+
+    return render(request, 'schoolsadmin/schools_details.html', context)
