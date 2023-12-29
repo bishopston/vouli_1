1992f050dc3d867721f15048764853288856af25
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2023-12-30 01:04:30 +0200

    reservations

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AdminDashboardView
 
 app_name = 'base'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('admin_dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'base/index.html'
+
+class AdminDashboardView(TemplateView):
+    template_name = 'base/admin_dashboard.html'
